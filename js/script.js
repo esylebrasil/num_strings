d637854bd4392eade4d39813ba2079a94b028de5
Elyse Masterson <elyselani@gmail.com>
2017-04-19 05:45:55 -0700

    Initial code for homework week2

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var allStrings []; 
+var total = 0; 
+var sum = 0; 
+
+$(#'submit').on('click', startAdd); 
+$(#reset').on('click', clearForm); 
+  
+function startAdd() {
+  var addData = $("#textInput").val();
+  if ($.isNumeric(addData)){
+    total ++; 
+    sum += parseFloat(addData); 
+    updateNumbers(); 
+  }
+  else {
+    allStrings.push(addData); 
+    updateText(addData); 
+  }
+  $('#textInput').val(''); 
+  event.preventDefault(); 
+} 
+
+function updateNumbers () {
+  var average = sum/total; 
+  if(isNaN(average)) {
+    average = 0; 
+  }
+  $('#count').text(total); 
+  $('#average').text(average); 
+  $('#sum').text(sum); 
+}
